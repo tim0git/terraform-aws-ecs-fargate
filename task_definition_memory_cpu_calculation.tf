f4bda3148c4ba7a1b9b6b3cf11def4bc642ed4b6
tim doolan <timothy.doolan@awaze.co.uk>
2023-04-25 11:38:07 +0100

    fix: cpu allocation.

--- task_definition_memory_cpu_calculation.tf
+++ task_definition_memory_cpu_calculation.tf
@@ -3,7 +3,7 @@ locals {
   firelense_log_agent_cpu_allocation            = floor(var.task_definition_memory_cpu_configuration.cpu * try(var.side_car_resource_allocation_configuration.firelense_log_agent.cpu, 0.125))
   new_relic_infrastructure_agent_cpu_allocation = floor(var.task_definition_memory_cpu_configuration.cpu * try(var.side_car_resource_allocation_configuration.new_relic_infrastructure_agent.cpu, 0.125))
   app_config_agent_cpu_allocation               = floor(var.task_definition_memory_cpu_configuration.cpu * try(var.side_car_resource_allocation_configuration.app_config_agent.cpu, 0.125))
-  xray_daemon_cpu_allocation                    = floor(var.task_definition_memory_cpu_configuration.cpu * try(var.side_car_resource_allocation_configuration.xray_daemon.cpu, 0.625))
+  xray_daemon_cpu_allocation                    = floor(var.task_definition_memory_cpu_configuration.cpu * try(var.side_car_resource_allocation_configuration.xray_daemon.cpu, 0.0625))
   calculated_application_cpu_allocation         = floor(var.task_definition_memory_cpu_configuration.cpu - sum([for side_car in local.side_cars : side_car.cpu]))
 
   reverse_proxy_memory_allocation                  = floor(var.task_definition_memory_cpu_configuration.memory * try(var.side_car_resource_allocation_configuration.reverse_proxy.memory, 0.0625))
